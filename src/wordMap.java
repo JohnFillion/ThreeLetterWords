c2aa650fc64a46f1b83a977e8d9b0b9d12905a27
John Fillion <60233839+JohnFillion@users.noreply.github.com>
2021-12-19 10:09:31 -0700

    Completed compareWords function.

---
+++ src/wordMap.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+public class wordMap {
+    /**
+     * Function to determine whether the source word and target word differ by only
+     * one letter.
+     * @param source Word that all other words will be compared to.
+     * @param target Current word that the target word is being compared to.
+     * @return
+     */
+    private static boolean compareWords(String source, String target) {
+        int wordLength = source.length();
+        int comparisonNum = wordLength - 1;
+        int similarityNum = 0;
+        for (int i = 0; i < wordLength; i++) {
+            if (Character.compare(source.charAt(i),target.charAt(i)) == 0) {
+                similarityNum += 1;
+                System.out.println("Success");
+            } else {
+                similarityNum += 0;
+            }
+        }
+        if (similarityNum == comparisonNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(compareWords("rat", "cat"));
+    }
+
+}
